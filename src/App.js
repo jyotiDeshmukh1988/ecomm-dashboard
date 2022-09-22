a819113c8a07b761df2bff21249fd5ccb7abd329
jyotiDeshmukh1988 <83502437+jyotiDeshmukh1988@users.noreply.github.com>
2022-09-22 05:18:01 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import UpdateProduct from "./components/UpdateProduct";
+import AddProduct from "./components/AddProduct";
+import NoPage from "./components/NoPage";
+import Login from './components/Login';
+import Register from './components/Register';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="updateproduct" element={<UpdateProduct />} />
+          <Route path="addproduct" element={<AddProduct />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Layout.js
@@ -0,0 +1,28 @@
+import { Outlet, Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+const Layout = () => {
+  return (
+    <>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Link to="/" className="mainLogo">React-Bootstrap</Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+              <Link to="/">Home</Link>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+              <Link to="/addproduct">Add Product</Link>
+              <Link to="/updateproduct">Update Product</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      <Outlet />
+    </>
+  )
+};
+
+export default Layout;
